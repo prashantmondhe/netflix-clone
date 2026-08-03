d0016606532246433b73f8f9a765e2f2f5c84527
prashantmondhe <mondheprashant@gmail.com>
2026-08-03 16:50:25 +0530

    6th commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { Provider } from 'react-redux';
+import { store } from './redux/store'; // आपण बनवलेला स्टोअर
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  
+    
+  
+);
